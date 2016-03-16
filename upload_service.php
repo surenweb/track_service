28c4db4cf6b6c34e5a1b8e609637791916865694
Suren <rover_raj@yahoo.com>
2016-03-16 16:40:30 +0545

    Image upload service

--- upload_service.php
+++ upload_service.php
@@ -10,9 +10,10 @@
 	}
 	$website=new website; $db=$website->connect(); $website=null; 
 	
+	
 	//## Upload Patrol 
 	if($task=="patrol")
-	{
+	{		
 		$sql = " INSERT INTO patrol (RID,MobileID,CaseTitle,PatrolType,PatrolOn,TotalNafri,StartDate,EndDate) 
 					VALUES ".$data ;					
 		$stmt=$db->prepare($sql);
@@ -30,8 +31,8 @@
 	
 	//Upload Track 
 	if($task=="track")
-	{	
-		$sql = " INSERT INTO patrol_track (RID,PatrolID,Lat,Lon,GpsDate,Accuracy) VALUES ".$data ;			
+	{	 
+		$sql = " INSERT INTO patrol_track (RID,MobileID,PatrolID,Lat,Lon,GpsDate,Accuracy) VALUES ".$data ;			
 		$stmt=$db->prepare($sql);
 		$op = $stmt->execute();		
 		if($op)
@@ -44,11 +45,47 @@
 		}	
 	}
 	
+	//Upload event 
+	if($task=="event")
+	{	
+		$sql = " INSERT INTO event (RID,MobileID,Event,EventDetail,EventImage,Lat, Lon, Accuracy,CreatedDate)".
+				" VALUES ".$data ;			
+		$stmt=$db->prepare($sql);
+		$op = $stmt->execute();		
+		if($op)
+		{	echo "1: inserted in database ."; 
+		}
+		else
+		{	
+			echo "0: Unable to insert data.";
+			KeepLog($sql);			
+		}	
+	}
+	
+	//Upload Image 
+	if($task=="image")
+	{			
+		$file_name = isset($_POST['file_name'])? $_POST['file_name'] : date("Y_m_d_His");
+		
+		try {
+			$binary=base64_decode($data);
+			header('Content-Type: bitmap; charset=utf-8');
+			$file = fopen('uploads/'.$file_name.'.jpg', 'wb');
+			fwrite($file, $binary);
+			fclose($file);
+			echo '1:Image upload complete!! ';
+		}		
+		catch(Exception $e) {
+			echo '0: Error writing image :' .$e->getMessage();
+		}		
+		
+	}
 	
 	//KEEP Un-Successful Queries in log 
 	function KeepLog($strQuery)
 	{
 		//Create Log With Query 
+		$website=new website; $db=$website->connect(); $website=null; 
 		$errSql = str_replace("'","#Q#",$strQuery);		
 		$logSql = "insert into v_error_log (ErrorSql) values ('".$errSql."')";
 		$stmt=$db->prepare($logSql);
